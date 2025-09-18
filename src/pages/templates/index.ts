5e8f6d003a9aabadb8e377920d2ccf5add26bcf4
AriadnaRamirez <ariadnamts98@gmail.com>
2025-09-18 12:19:17 -0600

    fix(index): Correcion de importacion index

--- src/pages/templates/index.ts
+++ src/pages/templates/index.ts
@@ -1,4 +1,4 @@
-export { default as TemplatesIndex } from './Index';
+export { default as TemplatesIndex } from './Index.tsx';
 export { default as TemplatesCreate } from './Create';
 export { default as TemplatesShow } from './Show';
 export { default as TemplatesEdit } from './Edit';
