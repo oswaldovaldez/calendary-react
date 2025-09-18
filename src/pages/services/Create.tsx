66fb666f5a81cc4e1e875fd371e80b8f3e09b95b
AriadnaRamirez <ariadnamts98@gmail.com>
2025-09-18 12:46:20 -0600

    feat(services): Implementacion del form service y el componente CreateService

--- src/pages/services/Create.tsx
+++ src/pages/services/Create.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Api } from "../../services/api";
+import { useAuthStore } from "../../store/auth.store";
+import { useNavigate } from "@tanstack/react-router";
+import type { ServiceFormValues } from "./FormService";
+import FormService from "./FormService";
 
-const Create = () => {
-  return (
-    <div>Create</div>
-  )
-}
+const CreateService = () => {
+	const token = useAuthStore((s) => s.token);
+	const navigate = useNavigate();
 
-export default Create
+	const initialValues: ServiceFormValues = {
+		name: "",
+		description: "",
+		commerce_id: 0,
+		duration: 0,
+		price: 0,
+		price_offer: 0,
+		_token: token ?? "",
+	};
+
+	const handleSubmit = async (values: ServiceFormValues) => {
+		try {
+			await Api.createService(values);
+			alert("Servicio creado con éxito");
+			navigate({ to: "/services" });
+		} catch (error) {
+			console.error("Error al crear servicio:", error);
+			alert("Error al crear servicio");
+		}
+	};
+
+	return (
+		<FormService initialValues={initialValues} onSubmit={handleSubmit} />
+	);
+};
+
+export default CreateService;
