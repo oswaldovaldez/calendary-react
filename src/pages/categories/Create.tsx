2d2f579e707df6f32968e406a1fe3c6db659bff5
AriadnaRamirez <ariadnamts98@gmail.com>
2025-10-21 18:42:22 -0600

    feat(ErrorForm): Componente que retorna los errores arrojados por el backend al hacer POST fallido. El componente se encuentra en ErrorForm.tsx y la funcion que maneja el error de la API esta en handleFormErrorApi

--- src/pages/categories/Create.tsx
+++ src/pages/categories/Create.tsx
@@ -5,43 +5,42 @@ import { useNavigate } from "@tanstack/react-router";
 import { useNotificationStore } from "../../store/notification.store";
 
 const CreateCategory = () => {
-  const token = useAuthStore((s) => s.token);
-  const commerce = useAuthStore((s) => s.commerce);
-  const navigate = useNavigate();
-  const notify = useNotificationStore((state) => state.notify);
-  const currentCommerceId = commerce?.id ?? 0;
+	const token = useAuthStore((s) => s.token);
+	const commerce = useAuthStore((s) => s.commerce);
+	const navigate = useNavigate();
+	const notify = useNotificationStore((state) => state.notify);
+	const currentCommerceId = commerce?.id ?? 0;
 
-  const initialValues: CategoryFormValues = {
-    name: "",
-    description: "",
-    parent_id: null,
-    commerce_id: currentCommerceId,
-  };
+	const initialValues: CategoryFormValues = {
+		name: "",
+		description: "",
+		parent_id: null,
+		commerce_id: currentCommerceId,
+	};
 
-  const handleSubmit = async (values: CategoryFormValues) => {
-    const payload: CategoryFormValues = {
-      ...values,
-      name: values.name.trim().toUpperCase(),
-      parent_id: null,
-      commerce_id: currentCommerceId,
-    };
+	const handleSubmit = async (values: CategoryFormValues) => {
+		const payload: CategoryFormValues = {
+			...values,
+			name: values.name.trim().toUpperCase(),
+			parent_id: null,
+			commerce_id: currentCommerceId,
+		};
 
-    Api.createCategory({
-      ...payload,
-      _token: `${token}`,
-    })
-      .then((res) => {
-        notify("success", res.message);
+		try {
+			const res = await Api.createCategory({
+				...payload,
+				_token: `${token}`,
+			});
+			notify("success", res.message);
+			navigate({ to: "/categories" });
+		} catch (error: any) {
+			throw error; // RE-lanzar el error hacia FormCategory
+		}
+	};
 
-        navigate({ to: "/categories" });
-      })
-
-      .catch((error: any) => {
-        console.error(error);
-      });
-  };
-
-  return <FormCategory initialValues={initialValues} onSubmit={handleSubmit} />;
+	return (
+		<FormCategory initialValues={initialValues} onSubmit={handleSubmit} />
+	);
 };
 
 export default CreateCategory;
